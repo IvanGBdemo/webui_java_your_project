80e54feb7cdf5ebdab25d9253e1391c706dbed1a
IvanGBdemo <82132183+IvanGBdemo@users.noreply.github.com>
2021-09-03 05:30:40 +0300

    added

---
+++ practical_work3/LogInToYourAccount.java
@@ -0,0 +1,33 @@
+package practical_work3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LogInToYourAccount {
+
+// Объявляю переменную драйвера
+    private static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+// Создаю код выполнения сценария
+        WebDriverManager.chromedriver().setup();
+// Инициализирую драйвер
+        driver = new ChromeDriver();
+        loginToMvideo();
+
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+
+// Создаю метод для входа на сайт
+    public static void loginToMvideo() {
+        driver.get("https://www.mvideo.ru");
+        driver.findElement(By.id("login-original")).sendKeys("jokovich85@mail.ru");
+        driver.findElement(By.id("login_password")).sendKeys("Мвидео21");
+        driver.findElement(By.id("submit-button")).click();
+    }
+}
